45d319b474a8606e0fa6ebd9b453dc81900189c7
Benjamin Esse <benjamin.esse@manchester.ac.uk>
2019-05-13 13:54:06 +0100

    Trimmed the status timestamp

--- openso2/station_status.py
+++ openso2/station_status.py
@@ -14,5 +14,5 @@ def get_station_status(self, station):
     # Try to retrieve the station status
     time, status = self.stat_com[station].pull_status()
 
-    self.station_widjets[station]['status_time'].set(time)
+    self.station_widjets[station]['status_time'].set(time[:-7])
     self.station_widjets[station]['status'].set(status)
